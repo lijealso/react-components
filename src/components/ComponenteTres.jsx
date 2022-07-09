417a24ad9abc1389fa9fb9e32a2fe59caba3c127
lijealso <lijealso@gmail.com>
2022-07-09 21:38:53 +0100

    add component jsx file

---
+++ src/components/ComponenteTres.jsx
@@ -0,0 +1,8 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from 'react'
+
+export default props =>
+        <div className="componente">
+            <p>Título: {props.titulo}</p>
+        </div>
+
