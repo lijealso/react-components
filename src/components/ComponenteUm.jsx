3d3efd7acfe1d4cc74ab6c5668992fe9096e6c2a
lijealso <lijealso@gmail.com>
2022-07-09 20:32:27 +0100

    new component

---
+++ src/components/ComponenteUm.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function ComponenteUm() {
+    return (
+            <div class="componente">
+                <p>Componente Um</p>
+                <p>Componente mais simples de React</p>
+            </div>
+    )
+}
+
+export default ComponenteUm
