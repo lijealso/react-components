5ac39bb56c0c3cd3bd69032c23271c2e7c518fda
lijealso <lijealso@gmail.com>
2022-07-09 20:32:00 +0100

    new App.js

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+
+// components
+import ComponenteUm from './components/ComponenteUm'
+
+function App() {
+    return (
+        <>
+            <ComponenteUm />  
+        </>
+    )
+}
+
+export default App
