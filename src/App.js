c107a2766b2ebdf5c4e22fbba090337b6f0c06e0
lijealso <lijealso@gmail.com>
2022-07-09 21:37:54 +0100

    add component 1 and 2

--- src/App.js
+++ src/App.js
@@ -3,10 +3,14 @@ import './App.css'
 
 // components
 import ComponenteUm from './components/ComponenteUm'
+import ComponenteDois from './components/ComponenteDois'
+import ComponenteTres from './components/ComponenteTres'
 
 function App() {
     return (
         <>
+            <ComponenteTres titulo="teste"/>
+            <ComponenteDois />
             <ComponenteUm />  
         </>
     )
