c96aae374027bd966c20098ec77dca5709399e42
lijealso <lijealso@gmail.com>
2022-07-12 15:44:14 +0100

    add component

--- src/App.js
+++ src/App.js
@@ -5,11 +5,19 @@ import './App.css'
 import ComponenteUm from './components/ComponenteUm'
 import ComponenteDois from './components/ComponenteDois'
 import ComponenteTres from './components/ComponenteTres'
+import ComponenteQuatro from "./components/ComponenteQuatro";
 
 function App() {
     return (
         <>
-            <ComponenteTres titulo="teste"/>
+            <ComponenteQuatro titulo="Componente Quatro">
+                <ul>
+                    <li>Item 1</li>
+                    <li>Item 2</li>
+                    <li>Item 3</li>
+                </ul>
+            </ComponenteQuatro>
+            <ComponenteTres titulo="teste" subtitulo="Texto do componente 3"/>
             <ComponenteDois />
             <ComponenteUm />  
         </>
