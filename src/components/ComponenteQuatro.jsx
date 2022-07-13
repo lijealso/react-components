bc49ba26f57a91a9b8233ee077fa0a0aef105641
lijealso <lijealso@gmail.com>
2022-07-13 23:45:46 +0100

    use component 5 inside

--- src/components/ComponenteQuatro.jsx
+++ src/components/ComponenteQuatro.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
+import ComponenteCinco from "./ComponenteCinco";
 
 const ComponenteQuatro = (props) => {
     return (
-        <div className="componente">
-            <p>Título: {props.titulo}</p>
-            {props.children}
-        </div>
-    )
+      <div className="componente">
+        <p>Título: {props.titulo}</p>
+        <ComponenteCinco texto="texto do componente cinco" />
+        <ComponenteCinco texto="texto do componente cinco" />
+        <ComponenteCinco texto="texto do componente cinco" />
+      </div>
+    );
 }
 export default ComponenteQuatro
