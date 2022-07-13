5db17d0bb3e24f1b7210207a5bb81cb31fb01052
lijealso <lijealso@gmail.com>
2022-07-13 23:46:33 +0100

    adjust component 4

--- src/App.js
+++ src/App.js
@@ -5,23 +5,17 @@ import './App.css'
 import ComponenteUm from './components/ComponenteUm'
 import ComponenteDois from './components/ComponenteDois'
 import ComponenteTres from './components/ComponenteTres'
-import ComponenteQuatro from "./components/ComponenteQuatro";
+import ComponenteQuatro from './components/ComponenteQuatro'
 
 function App() {
     return (
-        <>
-            <ComponenteQuatro titulo="Componente Quatro">
-                <ul>
-                    <li>Item 1</li>
-                    <li>Item 2</li>
-                    <li>Item 3</li>
-                </ul>
-            </ComponenteQuatro>
-            <ComponenteTres titulo="teste" subtitulo="Texto do componente 3"/>
-            <ComponenteDois />
-            <ComponenteUm />  
-        </>
-    )
+      <>
+        <ComponenteQuatro titulo="Componente Quatro" />
+        <ComponenteTres titulo="teste" subtitulo="Texto do componente 3" />
+        <ComponenteDois />
+        <ComponenteUm />
+      </>
+    );
 }
 
 export default App
